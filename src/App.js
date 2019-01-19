ffd8a2bcfbe951cdcbe8034a8097a372630626f4
Christian Marcantel <c.marcantel@live.com>
2019-01-19 17:09:44 -0600

    Let's try Grommet instead of a simple-flexbox
    Think _BIG_

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
-import { Label } from './components/Label';
-import { Box } from './components/Box';
-import { Column, Row } from 'simple-flexbox';
+import { Box, Label } from './components';
+import { Grommet } from 'grommet';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Column flexGrow={1}>
-          <Row horizontal='center' className="App-body">
-            <Box>
-              <Label>
-                CoA
-              </Label>
-            </Box>
-          </Row>
-        </Column>
-      </div>
+      <Grommet className="App-body">
+          <Box>
+            <Label>
+              CoA
+            </Label>
+          </Box>
+      </Grommet>
     );
   }
 }
