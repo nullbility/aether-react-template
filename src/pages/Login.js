fd1248964594c79087ef861e3de821fd0f0abbab
Christian Marcantel <c.marcantel@live.com>
2019-03-19 14:30:04 -0500

    Formated CSS grid for Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,66 +1,44 @@
 import React from 'react';
-import { 
-  Box, 
-  Button, 
-  Input, 
-  Grid,
-  Label 
-} from '../components';
+import { Box, Button, Input, Grid } from '../components';
 
 const Login = () => {
-
   return (
     <Grid
       fill
-      rows={["small", "medium", "flex"]}
-      columns={["flex", "medium", "flex"]}
+      rows={['flex', 'flex', 'flex']}
+      columns={['flex', 'medium', 'flex']}
       areas={[
-        { name: "header", start: [0, 0], end: [2, 0] },
-        { name: "body", start: [1, 1], end: [1, 1] },
-        { name: "footer", start: [0, 2], end: [2, 2] },
+        { name: 'header', start: [0, 0], end: [2, 0] },
+        { name: 'body', start: [1, 1], end: [1, 1] },
+        { name: 'footer', start: [0, 2], end: [2, 2] }
       ]}
     >
-
       <Box
         background={{
           color: 'neutral-1'
         }}
-        direction='column'
-        gridArea='body'
-        pad='small'
-        round='small'
+        direction="column"
+        gridArea="body"
+        height="fit-content"
+        pad="small"
+        round="small"
       >
-
         <Grid
-          rows={['small', 'small']}
+          rows={['auto', 'xsmall', 'auto']}
           columns={['flex', 'flex']}
           areas={[
-            { name: 'fields', start: [0,0], end: [1,0] },
-            { name: 'buttons', start: [0,1], end: [1,1] },
+            { name: 'fields', start: [0, 0], end: [1, 0] },
+            { name: 'buttons', start: [0, 2], end: [1, 2] }
           ]}
         >
-
-          <Box
-            gridArea='fields'
-            gap='small'
-          >
-            <Input 
-              placeholder='username'
-            />
-            <Input
-              placeholder='password'
-              type='password'
-            />
+          <Box gridArea="fields" gap="small">
+            <Input placeholder="username" />
+            <Input placeholder="password" type="password" />
           </Box>
 
-          <Box
-            gridArea='buttons'
-          >
-            <Button 
-              label='submit'
-            />
+          <Box gridArea="buttons">
+            <Button label="submit" />
           </Box>
-
         </Grid>
       </Box>
     </Grid>
